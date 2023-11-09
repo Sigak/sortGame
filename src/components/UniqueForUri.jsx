dbab0f5b6fce58a6b9614d834663d242db9e755d
sigak <anastasia-rggu@yandex.ru>
2023-11-10 01:51:21 +0300

    Updates

--- src/components/UniqueForUri.jsx
+++
@@ -1,9 +0,0 @@
-import {useParams} from "react-router-dom";
-
-export default function UniqueForUri(Component, paramName) {
-    return () => {
-        let params = useParams();
-
-        return (<Component key={params[paramName]}/>);
-    };
-}
